992bf45a9a6e3105d6efd36248f5a194419ea134
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-09-08 06:37:36 -0500

    Proyecto con dependencias y CRUD de Pokemon con enum

---
+++ src/main/java/com/juanma/pokemon/service/PokemonService.java
@@ -0,0 +1,30 @@
+package com.juanma.pokemon.repository;
+
+import com.juanma.pokemon.model.Pokemon;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PokemonService {
+
+    @Autowired
+    PokemonRepository pokemonRepository;
+
+    public void crearYActualizarPokemon(Pokemon pokemon){
+        pokemonRepository.save(pokemon);
+    }
+
+    public List<Pokemon> verPokemon(){
+        List<Pokemon> pokemons = new ArrayList<Pokemon>();
+        pokemons.addAll(pokemonRepository.findAll());
+        return pokemons;
+    }
+
+    public void eliminarPokemon(Long id){
+        pokemonRepository.deleteById(id);
+    }
+
+}
