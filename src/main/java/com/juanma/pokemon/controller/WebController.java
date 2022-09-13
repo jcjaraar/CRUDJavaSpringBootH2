9a09ab939a390eff240163ae03c6aa5a94795c3a
IngJuanMaSuarez <ingjuanmasuarez@gmail.com>
2022-09-13 06:17:19 -0500

    Esquema inicial para integrar front y back

--- src/main/java/com/juanma/pokemon/controller/WebController.java
+++ src/main/java/com/juanma/pokemon/controller/WebController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-//    @GetMapping("/index")
-//    public String index(){
-//        return "index";
-//    }
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
 
 }
